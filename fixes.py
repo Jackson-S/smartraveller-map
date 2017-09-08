45c00fd80f1920a79b71c630de5090d2d2d82274
Jackson Sommerich <github+jackson.sommerich@gmail.com>
2017-09-08 13:48:11 +1000

    Refactoring
    Removed reliance on pyCurl, replacing it with the native
    urllib.requests package.

---
+++ fixes.py
@@ -0,0 +1,35 @@
+""" This file contains the non-standard names of countries used by
+    smartraveller, mapping them to their standard 2 letter identifier
+"""
+
+country_fixes = {
+    'Democratic Republic of the Congo': 'CD',
+    'Republic of Korea': 'KR',
+    'Netherlands Antilles': 'AN',
+    'Reunion': 'RE',
+    'The Bahamas': 'BS',
+    'Iran': 'IR',
+    'Israel, the Gaza Strip and the West Bank': 'IL',
+    'Kosovo': 'XK',
+    'Laos': 'LA',
+    'Macau': 'MO',
+    'Syria': 'SY',
+    'Russia': 'RU',
+    'The Gambia': 'GM',
+    'Vietnam': 'VN',
+    'The Republic of the Congo': 'CG',
+    'South Korea': 'KR',
+    'North Korea': 'KP',
+    'Israel and the Occupied Palestinian Territories': 'IL',
+    'Macau (China)': 'MO',
+    "Cote d'Ivoire/Ivory Coast": 'CI',
+    'Cape Verde': 'CV',
+    'Hong Kong (China)': 'HK',
+    'Macedonia (Former Yugoslav Republic of Macedonia)': 'MK',
+    'St. Kitts and Nevis': 'KN',
+    'St. Lucia': 'LC',
+    'St. Vincent and the Grenadines': 'VC',
+    'The Netherlands': 'NL',
+    'Turks and Caicos': 'TC',
+    'Virgin Islands, US': 'VI'
+}
--- smartraveller.py
+++ smartraveller.py
@@ -1,95 +1,117 @@
-from sys import argv
-from re import search
-from io import BytesIO
+import os
+import re
 import json
+import argparse
 import pycountry
-import pycurl
+import urllib.request
 
-# Attempt to import cairosvg to output a png file
-world_map = "resources/map.svg"
-try:
-    import cairosvg
-    cairo = True
-    # Use the first argument if supplied as a scale for the png output
-    if len(argv) == 1:
-        scale = 2
-    else:
-        scale = int(argv[1])
-        # Set the world map to be a higher detail one if the scale is > 4x
-        if scale > 4:
-            world_map = "resources/map_hq.svg"
-        # If 0 is supplied disable png output
-        elif scale == 0:
-            cairo = False
-except ImportError:
-    cairo = False
+from fixes import country_fixes
 
-# for when smartraveller uses the wrong names or something goes wrong
-country_fixes = {
-    "Democratic Republic of the Congo": "CD", "Republic of Korea": "KR",
-    "Netherlands Antilles": "AN", "Reunion": "RE", "The Bahamas": "BS",
-    "Iran": "IR", "Israel, the Gaza Strip and the West Bank": "IL",
-    "Kosovo": "XK", "Laos": "LA", "Macau": "MO", "Syria": "SY", "Russia": "RU",
-    "The Gambia": "GM", "Vietnam": "VN", "The Republic of the Congo": "CG"
-}
+def get_country_list():
+    """ Retrieve a list of countries and statuses from smartraveller.gov.au """
+    url = "http://smartraveller.gov.au/countries/pages/list.aspx"
+    country_list_html = urllib.request.urlopen(url)
+    return country_list_html.read().decode("UTF-8")
 
-# The rgb vals of each warning (green, yellow, orange, red) smartraveller uses
-colours = dict(normal=(152, 211, 155), caution=(254, 230, 134),
-               warning=(249, 172, 95), danger=(229, 117, 116))
 
-# Retrieve the master list from smartraveller.gov.au using curl
-with BytesIO() as response:
-    c = pycurl.Curl()
-    c.setopt(c.URL, "http://smartraveller.gov.au/countries/pages/list.aspx")
-    c.setopt(c.WRITEDATA, response)
-    c.perform()
-    c.close()
-    utf8_response = response.getvalue().decode("UTF-8")
-
-# Parse the JSON data supplied by the url
-countries_json = search(r"CountryGrid.render\(.*\)", utf8_response)[0]
-countries_json = json.loads(countries_json[60:countries_json.rfind(")")])
-
-countries = list()
-
-# For each country in scraped JSON get the necessary fields into a nice array
-for index, item in enumerate(countries_json):
-    advisory = json.loads(item["Smartraveller_x0020_Advice_x0020_Levels"])
+def convert_to_png(scale):
     try:
-        ccode = pycountry.countries.lookup(item["Title"]).alpha_2
-    except LookupError:
-        ccode = country_fixes[item["Title"]]
-    advisory = advisory["items"]
-    if len(advisory) > 0:
-        for item in advisory:
-            if "overall" in item['text']:
-                advisory = item['level']
-                break
-    else:
-        advisory = None
-    countries.append((ccode, advisory))
+        import cairosvg
+    except BaseException as e:
+        print("{}\nCairosvg required to output PNG file!".format(e))
+        return
 
-# Create an array that will store all our override fill colours for the svg map
-code = ["#AU{fill: rgb(0, 0, 0);}"]
-for ccode, advisory in countries:
-    if advisory != None:
-        code.append("#{}{{fill:rgb({},{},{});}}".format(ccode, *colours[advisory]))
-
-# Read in the entire map resource
-with open(world_map) as map_file:
-    world_map = map_file.read()
-
-# Replace the python string with the code array we created, prepended with
-# correct indentation to make it slightly neater
-world_map = world_map.replace("<!-- PYTHON -->", "\n\t\t\t".join(code))
-
-with open("output.svg", "w") as out_file:
-    out_file.write(world_map)
-
-# Checks if cairosvg is installed and if so outputs a png version
-if cairo:
     cairosvg.svg2png(url="output.svg",
                      write_to="output.png",
                      parent_width=1024,
                      parent_height=660,
-                     scale=scale)
+                     scale=int(arguments.scale))
+
+
+def get_map_file(arguments):
+    if arguments.high_quality:
+        map_file = "hq.svg"
+    else:
+        map_file = "sq.svg"
+    return os.path.join("resources", map_file)
+
+
+def main(arguments):
+    # The rgb vals of each warning (green, yellow, orange, red) smartraveller uses
+    colours = { "normal":    (152, 211, 155),
+                "caution":   (254, 230, 134),
+                "warning":   (249, 172, 95),
+                "danger":    (229, 117, 116),
+                "australia": (0, 0, 0) }
+
+    # Find the correct JSON data in the page downloaded and read.
+    countries_json = json.loads(re.findall(r"\[{.*}\]", get_country_list())[2])
+
+    # Initialize a list of all countries on smartraveller
+    countries = [("AU", "australia")]
+
+    # For each country in scraped JSON get the necessary fields into a nice array
+    for index, item in enumerate(countries_json):
+        # Loads the advice levels for the specified country
+        advisory = json.loads(item["Smartraveller_x0020_Advice_x0020_Levels"])
+        # Attempt to look up country code using pycountry, on failure looks CC
+        # in country_fixes.
+        try:
+            ccode = pycountry.countries.lookup(item["Title"]).alpha_2
+        except LookupError:
+            ccode = country_fixes[item["Title"]]
+
+        # Obtain the current warning level for each country, if one exists
+        try:
+            advisory = advisory["items"][0]['level']
+        except IndexError:
+            advisory = None
+
+        countries.append((ccode, advisory))
+
+    # Read in the entire map resource (Done early to determine map code style)
+    with open(get_map_file(arguments)) as map_file:
+        world_map = map_file.read()
+
+    # Create an array that will store all our override fill colours for the svg map
+    code = list()
+
+    # Replace the python string with the code array we created, prepended with
+    # correct indentation to make it slightly neater
+    for ccode, advisory in countries:
+        if advisory != None:
+            ccolour = "rgb({},{},{})".format(*colours[advisory])
+            code.append("#{} {{fill: {};}}".format(ccode, ccolour))
+
+    world_map = world_map.replace("<!-- PYTHON Style1 -->", "\n".join(code))
+
+    # Write the output
+    with open("output.svg", "w") as out_file:
+        out_file.write(world_map)
+
+    # If conversion to png is required
+    if arguments.image:
+        convert_to_png(arguments.scale)
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+
+    args.add_argument("-q", "--high-quality",
+                      action="store_true",
+                      default=False,
+                      help="Use a high quality map file.")
+
+    args.add_argument("-i", "--image",
+                      action="store_true",
+                      default=False,
+                      help="Render a png version of the output.")
+
+    args.add_argument("-s", "--scale",
+                      action="store",
+                      default=1,
+                      help="Select the scale of the PNG output.")
+
+    arguments = args.parse_args()
+
+    main(args.parse_args())
